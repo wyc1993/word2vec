2b9bd5c592eb7cb3511bc0f980bcc67167136bf7
lmx <liumengxing2010@qq.com>
2015-04-05 16:45:45 +0800

    remove wtf

--- project/controllers/printer.py
+++ project/controllers/printer.py
@@ -1,12 +1,7 @@
 # -*- coding: utf-8 -*-
 from project import app
 from flask import render_template, request
-from flask.ext.wtf import Form, TextField, validators
 import json
-
-class CreateForm(Form):
-    text = TextField(u'Text:', [validators.Length(min=1, max=20)])
-
 
 @app.route('/')
 def start():
@@ -22,12 +17,3 @@ def get_vec():
     result = {"hehe": 0.8, "haha":0.7, "wangyici": 0.9, "lmx": 0.1, "cs": 0.3, "computer": 0.7, "movie": 0.3 }
     return json.dumps(result)
 
-@app.route('/print', methods=['GET', 'POST'])
-def printer():
-    form = CreateForm(request.form)
-    if request.method == 'POST' and form.validate():
-        from project.models.Printer import Printer
-        printer = Printer()
-        printer.show_string(form.text.data)
-        return render_template('printer/index.html')
-    return render_template('printer/print.html', form=form)
